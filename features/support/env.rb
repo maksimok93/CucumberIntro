774bda430cd6da78f76d4147c30eb249cf136ca0
maksimok93 <maksimok.dnepr@gmail.com>
2020-10-21 10:48:14 +0300

    Feat
    Add wait_visible_text method

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,10 @@
 require 'watir'
 require_relative '../../properties/credentials'
 
+def wait_visible_text(text)
+  Watir::Wait.until(timeout: 10) { @browser.text.include? text.to_s }
+end
+
 Before do
   @browser = Watir::Browser.new :chrome
   @browser.window.maximize
